e24768b6ba0bcb98479ff74d868581d5277e9248
Jmussleh <Jmussleh@gmail.comgit config --global user.name Jmussleh Jmussleh@gmail.com>
2023-03-26 22:22:38 -0400

    Adde all of the code for local storage in the script.js file

--- Assets/script.js
+++ Assets/script.js
@@ -44,3 +44,77 @@ var save8 = document.getElementById('save8');
 var save9 = document.getElementById('save9');
 
 var ConfirmEl = document.getElementById('Confirm');
+
+save1.addEventListener('click', function () {
+  var Task9E1 = document.getElementById('Task9');
+  var Task9E1 = Task9El.value;
+  localStorage.setItem('H9', Task9E1);
+  ConfirmEl.textContent =' The task was saved ';
+});
+// Save input and store it 2
+save2.addEventListener('click', function () {
+  var Task10E1 = document.getElementById('Task10');
+  var Task10E1 = Task10E1.value;
+  localStorage.setItem('H10', Task10E1);
+  ConfirmEl.textContent =' The task was saved ';
+});
+// Save input and store it 3
+save3.addEventListener('click', function () {
+  var Task11E1 = document.getElementById('Task11');
+  var Task11E1 = Task11El.value;
+  localStorage.setItem('H11', Task11E1);
+  ConfirmEl.textContent =' The task was saved ';
+});
+// Save input and store it 4
+save4.addEventListener('click', function () {
+  var Task12E1 = document.getElementById('Task12');
+  var Task12E1 = Task12El.value;
+  localStorage.setItem('H12', Task12E1);
+  ConfirmEl.textContent =' The task was saved ';
+});
+// Save input and store it 5
+save5.addEventListener('click', function () {
+  var Task1E1 = document.getElementById('Task1');
+  var Task1E1 = Task1El.value;
+  localStorage.setItem('H1', Task1E1);
+  ConfirmEl.textContent =' The task was saved ';
+});
+// Save input and store it 6
+save6.addEventListener('click', function () {
+  var Task2E1 = document.getElementById('Task2');
+  var Task2E1 = Task2El.value;
+  localStorage.setItem('H2', Task2E1);
+  ConfirmEl.textContent =' The task was saved ';
+});
+// Save input and store it 7
+save7.addEventListener('click', function () {
+  var Task3E1 = document.getElementById('Task3');
+  var Task3E1 = Task3El.value;
+  localStorage.setItem('H3', fTask3E1);
+  ConfirmEl.textContent =' The task was saved ';
+});
+// Save input and store it 8
+save8.addEventListener('click', function () {
+  var Task4E1 = document.getElementById('Task4');
+  var Task4E1 = Task4El.value;
+  localStorage.setItem('H4', Task4E1);
+  ConfirmEl.textContent =' The task was saved ';
+});
+// Save input and store it 9
+save9.addEventListener('click', function () {
+  var Task5E1 = document.getElementById('Task5');
+  var Task5E1 = Task5El.value;
+  localStorage.setItem('H5', Task5E1);
+  ConfirmEl.textContent =' The task was saved ';
+});
+
+// Text persists after refresh
+var Task9E1 = document.getElementById('Task9').value = Task9E1;
+var Task10E1 = document.getElementById('Task10').value = Task10E1;
+var Task11E1 = document.getElementById('Task11').value = Task11E1;
+var Task12E1 = document.getElementById('Task12').value = Task12E1;
+var Task1E1 = document.getElementById('Task1').value = Task1E1;
+var Task2E1 = document.getElementById('Task2').value = Task2E1;
+var Task3E1 = document.getElementById('Task3').value = Task3E1;
+var Task4E1 = document.getElementById('Task4').value = Task4E1;
+var Task5E1 = document.getElementById('Task5').value = Task5E1;
